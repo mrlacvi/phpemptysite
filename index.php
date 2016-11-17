493b80088c1a9094c23ba3a9f24a702a8652a12b
mrlacvi <mrlacvi@gmail.com>
2016-11-17 11:13:53 -0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,357 @@
-<?php
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="shortcut icon" href="../favicon.ico" >
+   <link rel="icon" type="image/gif" href="../animated_favicon1.gif" ><title>noticiastech.com.br - Quarta geração Intel Core i7 Extreme.</title>
 
-?>
+<meta name="description" content="Para a maioria das pessoas, um PC com processador Intel Core i5 ou AMD A6, ambos superam as necessidades diárias mas há 1% de usuários Hard que exige o máximo de desempenho para jogos e computação high-end. Pensando neste nicho Hard, a Intel desenvolveu o novo "Extreme Edition", Hexa-core i7-4960X, para os membros dessa elite. Os novos processadores estão no topo da lista de preços da Intel, podendo custar até 990 dólares somente o processador, não incluindo o cooler.
+Apesar de pertencer a já antiga série 4000, os novos chips Core i7 são baseados na microarquitetura Ivy Bridge da Intel, e não em Haswell. Isso significa que ele está restrito a placas-mãe com soquete 2011 e chipset Intel X79. A boa notícia para as pessoas que procuram atualizar seu Extreme anterior é que os novos chips são compatíveis com a Placa Mãe, mas você vai precisar atualizar a BIOS com um novo firmware. Essa opção irá lhe poupar algumas centenas de dólares, mas os fabricantes de placas-mãe irão forçar a venda de novos modelos.
+" />
+<meta name="keywords" content="display oled, intel extreme, 4 geração intel, asus, Hexa-core i7-4960X, amd, nvidia, pci-e,
+pci express, matozinhos mg, portal notícias tech, Android 4.4, key lime pie, android 5, Sundar Pichai, bateria, compacta, energia, superfina, display notebook, alto contraste, pretos profundos, noticias tech, noticias, tech, noticiastech, tecnologia, ofertas da internet, ofertas, superaquecimento," />
+<meta name="robots" content="index, follow" />
+<link href="../cssb/estilo.css" rel="stylesheet" type="text/css" media="screen" />
+<link rel="stylesheet" href="../slide/style.css" type="text/css">
+<script src="../slide/plugins/jquery-1.2.6.js" type="text/javascript"></script>
+<script src="../slide/plugins/jquery.cycle.all.js" type="text/javascript"></script>
+
+</head>
+<body>
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)[0];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = "//connect.facebook.net/pt_BR/all.js#xfbml=1";
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<div class="cabecalho">
+<div class="logo"> <img src="../logo2.jpg" /><!-- Insere a imagem do logo da pasta IMG --></div>
+<!-- Fim Logo -->
+<div class="menu">
+<table border="0" width="1080">
+<tbody>
+<tr>
+<td>
+<a href="../index.php">Home </a><!-- link menu -->
+<a href="../links.html"> Links úteis </a><!-- link menu -->
+<a href="/contato.html"> Contato </a><!-- link menu -->
+
+</td>
+<td style="font-size: 14px; font-weight: normal; color: red;" align="right"><?php $arquivo = "contador.txt" ; $ler_arquivo = fopen ($arquivo, 'r' ) ; $atual = fgets ($ler_arquivo); fclose ($ler_arquivo); $atualizar = fopen($arquivo, 'w'); fwrite ($atualizar, $atual + 1); echo "Você é o visitante número: " .$atual; ?>
+</td>
+</tr>
+</tbody>
+</table>
+</div>
+<!-- Fim menu --></div>
+<!-- Fim Cabeçalho -->
+<div class="conteudo">
+<div class="texto">
+<div style="width:740px">
+<p align="right" style="font-size: 10px;  align: "right"> Em 04 de setembro de 2013, Vagner Lacerda escreve para o portal Notícias Tech via Intel.</p>
+<P align="center">
+  <!-- LOMADEE - BEGIN -->
+  <script type="text/javascript" language="javascript">
+	lmd_source="27205825";
+	lmd_si="33755951";
+	lmd_pu="22648047";
+	lmd_c="BR";
+	lmd_wi="728";
+	lmd_he="90";
+  </script>
+  <script src="http://image.lomadee.com/js/ad_lomadee.js" type="text/javascript" language="javascript"></script>
+  <!-- LOMADEE - END -->
+</P>
+<p style="font-size: 16px; " align="center"> Intel apresenta a quarta geração dos processadores Core™ i7 Extreme
+</p>
+<p align="center"><img src="intel.jpg" width="730" height="318" /></p>
+
+
+<p align="justify"> Para a maioria das pessoas, um PC com processador Intel Core i5 ou AMD A6, ambos superam as necessidades diárias mas há 1% de usuários Hard que exige o máximo de desempenho para jogos e computação high-end. Pensando neste nicho Hard, a Intel desenvolveu o novo "Extreme Edition", Hexa-core i7-4960X, para os membros dessa elite. Os novos processadores estão no topo da lista de preços da Intel, podendo custar até 990 dólares somente o processador, não incluindo o cooler.</p>
+
+<p align="justify">Apesar de pertencer a já antiga série 4000, os novos chips Core i7 são baseados na microarquitetura Ivy Bridge da Intel, e não em Haswell. Isso significa que ele está restrito a placas-mãe com soquete 2011 e chipset Intel X79. A boa notícia para as pessoas que procuram atualizar seu  Extreme anterior é que os novos chips são compatíveis com a Placa Mãe, mas você vai precisar atualizar a BIOS  com um novo firmware. Essa opção irá lhe poupar algumas centenas de dólares, mas os fabricantes de placas-mãe irão forçar a venda de novos modelos.</p>
+
+<p align="justify">O clock base deste processador é de 3,6 GHz, mas esse número pode chegar a 4 GHz através do recurso Turbo Boost. A CPU possui seis núcleos físicos, mas, com a tecnologia Hyper Threading da Intel, ela pode realizar até 12 processos simultaneamente, já que o sistema operacional enxerga 12 núcleos lógicos. O processador também possui um cache L3 de 15 MB e suporta memória DDR3 1.866 MHz em modo quad channel. De acordo com a Intel, o novo Core i7-4960X apresentará um aumento de desempenho de cerca de 7% em jogos 3D, baseado no teste 3DMark do antigo Core i7-3960X Extreme Edition. A empresa ainda afirma que o novo Core i7-4960X é 36% mais rápido com jogos em 3D em comparação à arquitetura Haswell.</p>
+<p><img src="intel2.jpg" width="730" height="471" /></p> 
+<p align="justify"> Uma opção de Placa Mãe a ser utilizada é a Asus Rampage IV Extreme que possui slots PCIe para acomodar várias placas gráficas. O chipset X79 de estrutura LGA 2011 suporta PCIe 3.0, o suficiente para suportar várias placas gráficas high-end (usando a tecnologia CrossFire X da AMD ou SLI da Nvidia). Essa capacidade combinada com uma CPU high-end e muita memória, compõem um sistema "soberbo" de desempenho.</p>
+<p style="font-size: 14px; font-weight: bold;" align="center">Desvenda o significado das letras depois do número dos processadores</p>
+<p align="justify">Para facilitar a identificação dos modelos, a Intel apresenta uma letra após o número de modelo dos processadores. Veja o que cada sufixo significa:
+
+X: a letra X depois do código do processador significa que aquele modelo pertence à linha Extreme Edition, ou seja, os modelos mais poderosos. Todos possuem o clock destravado;
+K: o sufixo K significa que os processadores são top de linha, mas não os principais. Além disso, a letra também indica que o processador tem o clock destravado.
+Quando a CPU não traz nenhum sufixo ao lado do número, significa que é um modelo tradicional de processador, sem nenhuma característica especial. Todos esses modelos possuem o clock travado de fábrica.</p>
+<p><img src="intel3.png" width="730" height="378" /></p>
+<p align="justify">Se os entusiastas HI-TEC não se sentirem motivados a um upgrade, existe uma categoria que deve amar essa nova arquitetura:o segmento de servidores e estações de trabalho. Apesar de o desempenho não ser gritante em relação à geração anterior, o Ivy Bridge-E é incrivelmente mais eficiente que o seu antecessor, principalmente no que diz respeito ao consumo energético e geração de calor. uma máquina montada com um processador Intel Core i7-4960X utilizando quatro canais de memória não deixa de ser impressionante e rápida. Apesar do chipset X79 ser um pouco datado, ele é perfeitamente capaz de fornecer o suporte que a nova arquitetura precisa.</p>
+
+<p align="justify">Se você já possui uma máquina com um processador I7-3960X, não existem muitos motivos para trocar de processador por enquanto. Caso pretenda montar um computador absurdamente poderoso, o novo Core i7-4960X é a melhor opção.</div>
+  <?php
+$server = $_SERVER['SERVER_NAME'];
+$endereco = $_SERVER ['REQUEST_URI'];
+$urlatual = "http://".$endereco;
+?>             
+  
+
+<div class="fb-like" data-href="http://noticiastech.com.br/intel" data-width="450" data-show-faces="true" data-send="true">
+</div>   
+ 
+ <div class="fb-comments" data-href=<?php echo $urlatual;?>" https://facebook.com" data-width="750"></div>
+
+<p></p>
+<p></p>
+<p></p>
+</div>
+<!-- Fim Texto -->
+<div class="foto">
+<p style="font-size:16px;   "  align="center">Últimas notícias</p>
+<div class="foto2">
+
+    
+
+<table width="320" border="0">
+  <tr>
+    <td>
+<?php require("../slide/index.html"); ?>
+</td>
+  </tr>
+  <tr>
+    <td height="8"></td>
+  </tr>
+</table>  
+
+    <table width="310" border="0" padding-top="50">
+  <tr>
+    <td align="middle" bgcolor="#000000">
+
+    <div id='cse' style='width: 299px;' 'height: '>Loading</div>
+<script src='//www.google.com/jsapi' type='text/javascript'></script>
+<script type='text/javascript'>
+google.load('search', '1', {language: 'pt', style: google.loader.themes.V2_DEFAULT});
+google.setOnLoadCallback(function() {
+  var customSearchOptions = {};
+  var orderByOptions = {};
+  orderByOptions['keys'] = [{label: 'Relevance', key: ''} , {label: 'Date', key: 'date'}];
+  customSearchOptions['enableOrderBy'] = true;
+  customSearchOptions['orderByOptions'] = orderByOptions;
+  customSearchOptions['overlayResults'] = true;
+  var customSearchControl =   new google.search.CustomSearchControl('000972970936383103135:zmoutxf1qe0', customSearchOptions);
+  customSearchControl.setResultSetSize(google.search.Search.FILTERED_CSE_RESULTSET);
+  var options = new google.search.DrawOptions();
+  options.setAutoComplete(true);
+  customSearchControl.draw('cse', options);
+}, true);
+</script>
+<style type='text/css'>
+  .gsc-control-cse {
+    font-family: Arial, sans-serif;
+    border-color: black;
+    background-color: black;
+  }
+  .gsc-control-cse .gsc-table-result {
+    font-family: Arial, sans-serif;
+  }
+  input.gsc-input, .gsc-input-box, .gsc-input-box-hover, .gsc-input-box-focus {
+    border-color: #df6e09;
+  }
+  input.gsc-search-button, input.gsc-search-button:hover, input.gsc-search-button:focus {
+    border-color: #666666;
+    background-color: #df6e09;
+    background-image: none;
+    filter: none;
+
+  }
+  .gsc-tabHeader.gsc-tabhInactive {
+    border-color: #FF9900;
+    background-color: red;
+  }
+  .gsc-tabHeader.gsc-tabhActive {
+    border-color: #E9E9E9;
+    background-color: red;
+    border-bottom-color: #FF9900
+  }
+  .gsc-tabsArea {
+    border-color: #df6e09;
+  }
+  .gsc-webResult.gsc-result, .gsc-results .gsc-imageResult {
+    border-color: #FFFFFF;
+    background-color: #ccc;
+  }
+  .gsc-webResult.gsc-result:hover, .gsc-imageResult:hover {
+    border-color: #FFFFFF;
+    background-color: #ccc;
+  }
+  .gs-webResult.gs-result a.gs-title:link, .gs-webResult.gs-result a.gs-title:link b, .gs-imageResult a.gs-title:link, .gs-imageResult a.gs-title:link b  {
+    color: #0000CC;
+  }
+  .gs-webResult.gs-result a.gs-title:visited, .gs-webResult.gs-result a.gs-title:visited b, .gs-imageResult a.gs-title:visited, .gs-imageResult a.gs-title:visited b {
+    color: #0000CC;
+  }
+  .gs-webResult.gs-result a.gs-title:hover, .gs-webResult.gs-result a.gs-title:hover b, .gs-imageResult a.gs-title:hover, .gs-imageResult a.gs-title:hover b {
+    color: #0000CC;
+  }
+  .gs-webResult.gs-result a.gs-title:active, .gs-webResult.gs-result a.gs-title:active b, .gs-imageResult a.gs-title:active, .gs-imageResult a.gs-title:active b {
+    color: #0000CC;
+  }
+  .gsc-cursor-page {
+    color: #0000CC;
+  }
+  a.gsc-trailing-more-results:link {
+    color: #0000CC;
+  }
+  .gs-webResult .gs-snippet, .gs-imageResult .gs-snippet, .gs-fileFormatType {
+    color: #000000;
+  }
+  .gs-webResult div.gs-visibleUrl, .gs-imageResult div.gs-visibleUrl {
+    color: #008000;
+  }
+  .gs-webResult div.gs-visibleUrl-short {
+    color: #008000;
+  }
+  .gs-webResult div.gs-visibleUrl-short  {
+    display: none;
+  }
+  .gs-webResult div.gs-visibleUrl-long {
+    display: block;
+  }
+  .gs-promotion div.gs-visibleUrl-short {
+    display: none;
+  }
+  .gs-promotion div.gs-visibleUrl-long  {
+    display: block;
+  }
+  .gsc-cursor-box {
+    border-color: #FFFFFF;
+  }
+  .gsc-results .gsc-cursor-box .gsc-cursor-page {
+    border-color: #E9E9E9;
+    background-color: #ccc;
+    color: #0000CC;
+  }
+  .gsc-results .gsc-cursor-box .gsc-cursor-current-page {
+    border-color: #FF9900;
+    background-color: #ccc;
+    color: #0000CC;
+  }
+  .gsc-webResult.gsc-result.gsc-promotion {
+    border-color: #336699;
+    background-color: #ccc;
+  }
+  .gsc-completion-title {
+    color: #0000CC;
+  }
+  .gsc-completion-snippet {
+    color: #000000;
+  }
+  .gs-promotion a.gs-title:link,.gs-promotion a.gs-title:link *,.gs-promotion .gs-snippet a:link  {
+    color: #0000CC;
+  }
+  .gs-promotion a.gs-title:visited,.gs-promotion a.gs-title:visited *,.gs-promotion .gs-snippet a:visited {
+    color: #0000CC;
+  }
+  .gs-promotion a.gs-title:hover,.gs-promotion a.gs-title:hover *,.gs-promotion .gs-snippet a:hover  {
+    color: #0000CC;
+  }
+  .gs-promotion a.gs-title:active,.gs-promotion a.gs-title:active *,.gs-promotion .gs-snippet a:active {
+    color: #0000CC;
+  }
+  .gs-promotion .gs-snippet, .gs-promotion .gs-title .gs-promotion-title-right, .gs-promotion .gs-title .gs-promotion-title-right * {
+    color: #000000;
+  }
+  .gs-promotion .gs-visibleUrl,.gs-promotion .gs-visibleUrl-short  {
+    color: #008000;
+  }
+</style></td>
+  </tr>
+  <tr>
+    <td height:"8"></td>
+  </tr>
+    </table>
+    
+
+  <div id="nav">
+      
+      <script type="text/javascript">
+      <!--
+      i = 0;
+      tempo = 30;
+      tamanho = 250; // tamanho da barra de rolagem  >> Ver arquivo 
+
+Leiame.txt
+ 
+      function Rolar() {
+        document.getElementById('painel').scrollTop = i;
+        i++;
+        t = setTimeout("Rolar()", tempo);
+        if (i == tamanho) {
+          i = 0;
+        }
+      }
+      function Parar() {
+        clearTimeout(t);
+      }
+      //-->
+    </script>
+  
+ 
+  <body onload="Rolar()">
+    <div id="painel" style=" background-color: black; cursor: default; height: 190px; width: 
+
+310px; overflow: hidden;  " onmouseover="Parar()" onmouseout="Rolar
+
+()">
+      
+      <br/><br />
+      <?php require("../artigos/artigos.html"); ?>
+    <br />
+      <!-- FIM DO CONTEÚDO DA DIV -->
+    </div></div>
+   
+ 
+ <table width="320" border="0" cellspacing="10">
+  <tr >
+    <td>
+
+
+  <iframe src="http://www.tempoagora.com.br/selos_iframe/wide_BeloHorizonte-MG,SaoPaulo-SP,RiodeJaneiro-RJ.html" height="200" width="300" frameborder="0" allowtransparency="yes" scrolling="no"></iframe>
+  
+  <script type="text/javascript"><!--
+google_ad_client = "ca-pub-5989919999193359";
+/* vagner1 */
+google_ad_slot = "2966484029";
+google_ad_width = 300;
+google_ad_height = 280;
+//-->
+  </script>
+  </td>
+  </tr>
+  </table>
+  <table width="320" border="0" cellspacing="5">
+  <tr>
+  <td>
+  <script type="text/javascript"><!--
+google_ad_client = "ca-pub-5989919999193359";
+/* vagner1 */
+google_ad_slot = "2966484029";
+google_ad_width = 320;
+google_ad_height = 280;
+//-->
+</script>
+</td></tr></table>
+<table width="320" border="0" cellspacing="10">
+  <tr>
+  <td>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+<a href="http://www.hostgator.com.br/11632-0-1-20.html" target="_blank"><img src="http://afiliados.hostgator.com.br/banners/hospedagem_M_300x250.gif" alt="" border="0" height="250" width="300" /></a>
+  </td>
+  </tr>
+</table>
+ 
+</div>
+</div>
+<!-- Fim foto --> </div>
+<!-- Fim conteudo -->
+<div class="rodape"></div>
+<!-- Fim rodape -->
+</body></html>
